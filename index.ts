aed66a010346ef4d634f63ceedf5b912ad62ee7f
Swarit Choudhari <68472469+SwiftyProgrammer690@users.noreply.github.com>
2022-10-25 13:42:15 +0000

    bg change

--- index.ts
+++ index.ts
@@ -4,11 +4,11 @@ blog({
   title: "Will Robots Steal Our Jobs?",
   author: "Swarit Choudhari",
   avatar: "./avatar.svg",
-  avatarClass: "full",
+  avatarClass: "rounded-full",
   links: [
     { title: "Email", url: "mailto:swaritchoudhari.p.s.y@gmail.com" },
-    { title: "GitHub", url: "https://github.com/SwiftyProgrammer690" },
+    { title: "GitHub", url: "https://github.com/SwiftyProgrammer690/Passion-Project" },
   ],
-  background: "#fff",
+  background: "#BEBEBE",
   favicon: "favicon.ico",
 });
